a9028a5cabd56cd0b526a6ebf9e3343c74d63093
Marc Gruber <marc.gruber@ionesoft.ch>
2024-03-02 23:28:13 +0100

    add gamepad support

--- src/misc/Controls.ts
+++ src/misc/Controls.ts
@@ -1,6 +1,22 @@
 import Button = Phaser.Input.Gamepad.Button;
+import CursorKeys = Phaser.Types.Input.Keyboard.CursorKeys;
+import EventEmitter = Phaser.Events.EventEmitter;
 
 export class Controls {
+
+    static INPUT_ACTION_EVENT_KEY = 'inputAction';
+
+    /*
+    SPECIAL INPUT EVENTS
+    */
+    static INPUT_ACTION_EVENT_KEY_BUTTON_X = 'BUTTON_X';
+    static INPUT_ACTION_EVENT_KEY_BUTTON_Y = 'BUTTON_Y';
+    static INPUT_ACTION_EVENT_KEY_BUTTON_A = 'BUTTON_A';
+    static INPUT_ACTION_EVENT_KEY_BUTTON_B = 'BUTTON_B';
+    static INPUT_ACTION_EVENT_KEY_BUTTON_R = 'BUTTON_R1';
+    static INPUT_ACTION_EVENT_KEY_BUTTON_L = 'BUTTON_L1';
+    static INPUT_ACTION_EVENT_KEY_BUTTON_START = 'BUTTON_START';
+    static INPUT_ACTION_EVENT_KEY_BUTTON_SELECT = 'BUTTON_SELECT';
 
     private scene: Phaser.Scene;
 
@@ -22,58 +38,97 @@ export class Controls {
     private isButtonStart: boolean = false;
     private isButtonSelect: boolean = false;
 
+    private keyboardCursors: CursorKeys;
+
+    public inputActionEvent: EventEmitter;
 
     constructor(scene: Phaser.Scene) {
         this.scene = scene;
 
+        this.setupControlEvents();
+        this.setupGamePadControls();
+        this.setupKeyBoardControls();
+    }
+
+    private setupControlEvents() {
+        this.inputActionEvent = new Phaser.Events.EventEmitter();
+    }
+
+    private setupGamePadControls() {
+        /* connect gamepad if "connect" event is not fired */
         if (this.scene.input.gamepad.total) {
             this.pad = this.scene.input.gamepad.pad1;
         }
 
+        /* used to connect gamepad */
         this.scene.input.gamepad.once('down', (pad: Phaser.Input.Gamepad.Gamepad, button: object, index: number) => {
             this.pad = pad;
-            console.log('button ' + button + ' down');
         });
 
+        /* catch actual buttons */
         this.scene.input.gamepad.on('down', (pad: Phaser.Input.Gamepad.Gamepad, button: Button, index: number) => {
 
             if (button.index === 3) {
                 this.isButtonX = true;
+                this.inputActionEvent.emit(Controls.INPUT_ACTION_EVENT_KEY, Controls.INPUT_ACTION_EVENT_KEY_BUTTON_X);
             }
 
             if (button.index === 4) {
                 this.isButtonY = true;
+                this.inputActionEvent.emit(Controls.INPUT_ACTION_EVENT_KEY, Controls.INPUT_ACTION_EVENT_KEY_BUTTON_Y);
             }
 
             if (button.index === 0) {
                 this.isButtonA = true;
+                this.inputActionEvent.emit(Controls.INPUT_ACTION_EVENT_KEY, Controls.INPUT_ACTION_EVENT_KEY_BUTTON_A);
             }
 
             if (button.index === 1) {
                 this.isButtonB = true;
+                this.inputActionEvent.emit(Controls.INPUT_ACTION_EVENT_KEY, Controls.INPUT_ACTION_EVENT_KEY_BUTTON_B);
             }
 
             if (button.index === 7) {
                 this.isButtonR = true;
+                this.inputActionEvent.emit(Controls.INPUT_ACTION_EVENT_KEY, Controls.INPUT_ACTION_EVENT_KEY_BUTTON_R);
             }
 
             if (button.index === 6) {
                 this.isButtonL = true;
+                this.inputActionEvent.emit(Controls.INPUT_ACTION_EVENT_KEY, Controls.INPUT_ACTION_EVENT_KEY_BUTTON_L);
             }
 
             if (button.index === 11) {
                 this.isButtonStart = true;
+                this.inputActionEvent.emit(Controls.INPUT_ACTION_EVENT_KEY, Controls.INPUT_ACTION_EVENT_KEY_BUTTON_START);
             }
 
             if (button.index === 10) {
                 this.isButtonSelect = true;
+                this.inputActionEvent.emit(Controls.INPUT_ACTION_EVENT_KEY, Controls.INPUT_ACTION_EVENT_KEY_BUTTON_SELECT);
             }
         });
+    }
 
+    private setupKeyBoardControls() {
+        this.keyboardCursors = this.scene.input.keyboard.createCursorKeys()
+
+        this.scene.input.keyboard.on('keydown', (event: any) => {
+            if (event.keyCode === Phaser.Input.Keyboard.KeyCodes.SPACE) {
+                this.inputActionEvent.emit(Controls.INPUT_ACTION_EVENT_KEY, Controls.INPUT_ACTION_EVENT_KEY_BUTTON_A);
+            }
+
+        });
     }
 
     update() {
         this.isRight = this.isLeft = this.isUp = this.isDown = false;
+
+        this.updateGamePadControls();
+        this.updateKeyBoardControls();
+    }
+
+    private updateGamePadControls() {
 
         // gamePad controls
         if (this.pad) {
@@ -89,6 +144,26 @@ export class Controls {
             }
         }
     }
+
+    private updateKeyBoardControls() {
+
+        if (this.keyboardCursors.up.isDown) {
+            this.isUp = true;
+        }
+
+        if (this.keyboardCursors.down.isDown) {
+            this.isDown = true;
+        }
+
+        if (this.keyboardCursors.left.isDown) {
+            this.isLeft = true;
+        }
+
+        if (this.keyboardCursors.right.isDown) {
+            this.isRight = true;
+        }
+    }
+
 
     public noAxisIsPressed(): boolean {
         return !this.isUp && !this.isDown && !this.isLeft && !this.isRight;
--- src/scenes/menu-scene.ts
+++ src/scenes/menu-scene.ts
@@ -6,11 +6,11 @@ export class MenuScene extends Phaser.Scene {
 
     private logo: Phaser.GameObjects.Image;
 
-    private buttonNewGame: Phaser.GameObjects.Image;
+    private buttonNewGameImage: Phaser.GameObjects.Image;
 
-    private buttonControls: Phaser.GameObjects.Image;
+    private buttonControlsImage: Phaser.GameObjects.Image;
 
-    private selectionArrow: Phaser.GameObjects.Image;
+    private selectionArrowImage: Phaser.GameObjects.Image;
 
     private controls: Controls;
 
@@ -18,13 +18,9 @@ export class MenuScene extends Phaser.Scene {
 
     private currentSelectionIndex = 0;
 
-    private nextDelayStep: number = 0;
-
-    private INPUT_AXIS_DELAY: number = 200;
-
     private selections = [
-        {key: 'newGame', coord: new Point(292, 200), selected: true},
-        {key: 'controls', coord: new Point(292, 352), selected: false}
+        {key: 'newGame', coord: new Point(292, 302), selected: true},
+        {key: 'controls', coord: new Point(292, 350), selected: false}
     ];
 
     constructor() {
@@ -34,15 +30,25 @@ export class MenuScene extends Phaser.Scene {
     create() {
         this.controls = new Controls(this);
 
+        this.controls.inputActionEvent.addListener(Controls.INPUT_ACTION_EVENT_KEY, (key: string) => {
+
+            switch (key) {
+                case Controls.INPUT_ACTION_EVENT_KEY_BUTTON_A:
+                    this.scene.start('TileScene');
+                    break;
+            }
+
+        });
+
         const width = this.cameras.main.width;
         const height = this.cameras.main.height;
 
         this.add.image(0, 0, LoadingScene.UI_ATLAS_KEY, 'background.png').setOrigin(0, 0);
         this.logo = this.add.image(width * 0.5, 160, LoadingScene.UI_ATLAS_KEY, 'Logo.png');
-        this.buttonNewGame = this.add.image(width * 0.5, 302, LoadingScene.UI_ATLAS_KEY, 'new_game.png');
-        this.buttonControls = this.add.image(width * 0.5, 350, LoadingScene.UI_ATLAS_KEY, 'controls.png');
+        this.buttonNewGameImage = this.add.image(width * 0.5, 302, LoadingScene.UI_ATLAS_KEY, 'new_game.png');
+        this.buttonControlsImage = this.add.image(width * 0.5, 350, LoadingScene.UI_ATLAS_KEY, 'controls.png');
 
-        this.selectionArrow = this.add.image(292, 346, LoadingScene.UI_ATLAS_KEY, 'selection_arrow.png');
+        this.selectionArrowImage = this.add.image(292, 302, LoadingScene.UI_ATLAS_KEY, 'selection_arrow.png');
 
         this.tweens.add({
             targets: this.logo,
@@ -52,17 +58,23 @@ export class MenuScene extends Phaser.Scene {
             repeat: -1,
             ease: 'Sine.easeInOut'
         });
+
+        this.updateAxisSelection(0);
     }
 
     update(time: number, delta: number) {
         this.updateControls(time)
     }
 
-    private updateAxisSelection(dir: number, time: number) {
+    private updateAxisSelection(dir: number) {
 
         this.currentSelectionIndex += dir;
 
-        if (this.currentSelectionIndex < 0 || this.currentSelectionIndex > this.selections.length - 1) {
+        if (this.currentSelectionIndex < 0) {
+            this.currentSelectionIndex = this.selections.length - 1;
+        }
+
+        if (this.currentSelectionIndex > this.selections.length - 1) {
             this.currentSelectionIndex = 0;
         }
 
@@ -70,7 +82,7 @@ export class MenuScene extends Phaser.Scene {
             selection.selected = false;
             if (index === this.currentSelectionIndex) {
                 selection.selected = true;
-                this.selectionArrow.y = selection.coord.y;
+                this.selectionArrowImage.y = selection.coord.y;
             }
         })
     }
@@ -84,14 +96,14 @@ export class MenuScene extends Phaser.Scene {
 
         if (this.controls.up()) {
             if (!this.buttonIsDown) {
-                this.updateAxisSelection(-1, time)
+                this.updateAxisSelection(-1)
                 this.buttonIsDown = true;
             }
         }
 
         if (this.controls.down()) {
             if (!this.buttonIsDown) {
-                this.updateAxisSelection(1, time)
+                this.updateAxisSelection(1)
                 this.buttonIsDown = true;
             }
         }
