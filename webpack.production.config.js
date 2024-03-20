f11ba16891ff8946f0a9d741caaf4a4fec322661
Marc Gruber <marc.gruber@gmail.com>
2024-03-20 21:28:20 +0100

    add production webpack

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,12 +1,14 @@
 const path = require('path');
 const CopyPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/game.ts',
     mode: "production",
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
+        filename: 'bundle.js',
+        clean: true
     },
     module: {
         rules: [
@@ -23,9 +25,25 @@ module.exports = {
         ]
     },
     plugins: [
+        new HtmlWebpackPlugin({
+            filename: "index.html",
+            template: './src/index.html',
+            minify: {
+                removeAttributeQuotes: false,
+                collapseWhitespace: false,
+                html5: false,
+                minifyCSS: false,
+                minifyJS: false,
+                minifyURLs: false,
+                removeComments: false,
+                removeEmptyAttributes: false
+            },
+            hash: false
+
+        }),
         new CopyPlugin({
                 patterns: [
-                    {from: 'assets/**/*', to: ''}
+                    {from: 'assets/**/*', to: '', globOptions: {ignore: ['**/less/**']}}
                 ]
             }
         ),
