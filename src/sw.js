24772e0010f7510753ccfbe30897f6ce7ee70195
Marc Gruber <marc.gruber@ionesoft.ch>
2025-07-06 10:34:16 +0200

    add manifest.json

---
+++ src/sw.js
@@ -0,0 +1,17 @@
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open('v1').then((cache) => cache.addAll([
+            '/',
+            '/index.html',
+            '/styles.css',
+            '/script.js',
+            '/icons/icon-192.png'
+        ]))
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => response || fetch(event.request))
+    );
+});
--- webpack.production.config.js
+++ webpack.production.config.js
@@ -44,7 +44,9 @@ module.exports = {
         }),
         new CopyPlugin({
                 patterns: [
-                    {from: 'assets/**/*', to: '', globOptions: {ignore: ['**/less/**']}}
+                    {from: 'assets/**/*', to: '', globOptions: {ignore: ['**/less/**']}},
+                    {from: 'manifest.json', to: ''}, // PWA
+                    {from: 'src/sw.js', to: ''} // PWA
                 ]
             }
         ),
